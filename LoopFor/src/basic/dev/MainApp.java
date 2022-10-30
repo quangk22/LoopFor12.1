c599ea83d56b7d1fdcd8f392f1c2e1b236e2938e
quangk22 <113655587+quangk22@users.noreply.github.com>
2022-10-30 12:43:07 +0700

    Add files via upload

---
+++ LoopFor/src/basic/dev/MainApp.java
@@ -0,0 +1,15 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int tong = 0;
+		for(int i = 0 ; i <= 100 ; i++) {
+			tong +=i;
+			
+		}
+		System.out.println("Tổng từ 1 đến 100 :"+ tong);
+
+	}
+
+}
